2e658a7c75ecf4b6bde15a92d532d11649223bc2
German Vesi <gervesi@users.noreply.github.com>
2023-10-12 13:37:39 +0300

    vist 6 tund lõppend

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const dateInfo = require('./dateTimeFnc');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+//route
+app.get('/', (req, res)=>{
+	//res.send('See töötab!');
+	res.render('index');
+});
+
+app.get('/timenow', (req, res)=>{
+	const dateNow = dateInfo.dateNowET();
+	const timeNow = dateInfo.timeNowET();
+	res.render('timenow', {dateN: dateNow, timeN: timeNow});
+});
+
+app.get('/wisdom', (req, res)=>{
+	let folkWisdom = [];
+	fs.readFile("public/txtfiles/vanasonad.txt", "utf8", (err, data)=>{
+		if(err){
+			console.log(err);
+		}
+		else {
+			folkWisdom = data.split(";");
+			res.render('justlist', {h1: 'Vanasõnad'});
+		}
+	});
+});
+
+app.listen(5217);
